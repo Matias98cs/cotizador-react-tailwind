68e7bafb78749cde52a82c2b611182501f72e50b
Matias98cs <matias98cs@gmail.com>
2023-01-09 16:19:15 -0300

    muestra el resultado ya cotizado con el plan, marca y year

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { obtenerDiferenciaYear } from "../helpers";
+import { calcularMarca, calcularPlan, formatearDinero, obtenerDiferenciaYear } from "../helpers";
 
 const CotizadorContext = createContext();
 
@@ -24,15 +24,23 @@ const CotizadorProvider = ({ children }) => {
 
     //obtener diferencia de years
     const diferencia = obtenerDiferenciaYear(datos.year)
-    //hay que restar el 3% por cada year
-    resultado -= ((diferencia * 3) * resultado) /100
-    console.log(resultado)
-    //americano 15%
-    //europeo 30%
-    //asiatico 5%
 
-    //basico 20%
+    //hay que restar el 3% por cada year
+    resultado -= ((diferencia * 3) * resultado) / 100
+
+    //europeo 30%
+    //americano 15%
+    //asiatico 5%
+    resultado *= calcularMarca(datos.marca)
+
     //completo 50%
+    //basico 20%
+    resultado *= calcularPlan(datos.plan)
+    
+    //formatear dinero
+    resultado = formatearDinero(resultado)
+    console.log(resultado)
+
   }
 
   return (
