2c116132a710cc7ea7670ad4726177ae96d3a8aa
Matias98cs <matias98cs@gmail.com>
2023-01-09 14:56:34 -0300

    hook personalizado para trear el estado a los componentes

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,23 +1,24 @@
-import {createContext} from 'react'
+import { createContext, useState } from "react";
 
-const CotizadorContext = createContext()
+const CotizadorContext = createContext();
 
-const CotizadorProvider = ({children}) => {
+const CotizadorProvider = ({ children }) => {
+  //aca se puede definir lo que tu quieras para poner a disponer a todosl os componentes
+  const [modal, setModal] = useState(false)
+  const cambiarState = () => {
+    setModal(!modal)
+  }
 
+  return (
+    <CotizadorContext.Provider value={{
+        modal,
+        cambiarState
+    }}>
+        {children}
+    </CotizadorContext.Provider>
+  );
+};
 
-    return(
-        <CotizadorContext.Provider
-            value={{
-                
-            }}
-        >
-            {children}
-        </CotizadorContext.Provider>
-    )
-}
+export { CotizadorProvider };
 
-export {
-    CotizadorProvider
-}
-
-export default CotizadorContext
+export default CotizadorContext;
